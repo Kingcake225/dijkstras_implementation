86a5f805de9ab3681afbdda728e51277441917f4
Sean <62218565+Kingcake225@users.noreply.github.com>
2024-08-27 17:20:41 +0100

    Added a function: convertToAdjancency List.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import osmnx as ox
 import folium
+import json
+from shapely.geometry import mapping
 import config # Import later
 # Contains API key for Position Stack - converts an address to long and lat coords.
 # config.PS_apiKey to call
@@ -9,16 +11,21 @@ class Map:
     def __init__(self, start_coords, end_coords): # Start coords must be given at tuple
         self.start_coords = start_coords
         self.end_coords = end_coords
-        self.network = ox.graph_from_point(start_coords, dist=1000, network_type='drive') # dist is buffer distance
+        self.network = ox.graph_from_point(start_coords, dist=1000, network_type='drive') # dist is buffer distance, only road networks, doesn't account for walking
+
+    def convertToAdjacencyList(self):
+        nx.write_multiline_adjlist(self.network, 'network.adjlist')
 
     def findNearestNode(self):
+
         orig_node = ox.distance.nearest_nodes(self.network, self.start_coords[1], self.start_coords[0])
         dest_node = ox.distance.nearest_nodes(self.network, self.end_coords[1], self.end_coords[0])
         return orig_node, dest_node
 
-    def generate_shortest_map(self, orig_node, dest_node):
+    # Current implementation only shows shortest driving route, not walking, because shit takes to long to load otherwise
+    def generateShortestMap(self, orig_node, dest_node):
 
-        shortest_path = nx.shortest_path(self.network, orig_node, dest_node, weight='length') # Replace nx.shortest_path with own implementation of Dijkstra
+        shortest_path = nx.dijkstra_path(self.network, orig_node, dest_node, weight='length') # Replace nx.shortest_path with own implementation of Dijkstra
 
         m = folium.Map(location=self.start_coords, zoom_start=15)
 
@@ -40,11 +47,12 @@ class Map:
 
         m.save("shortest_path_map.html")
 
-
 # Origin location and destination
 start_coords = (51.45898602638651, -2.6188274814116506) # CHS
 end_coords = (51.45481197860214, -2.609414437680666) # QEH
 
 Map1 = Map(start_coords, end_coords)
 orig_node, dest_node = Map1.findNearestNode()
-Map1.generate_shortest_map(orig_node, dest_node)
+#Map1.generateShortestMap(orig_node, dest_node) # creates folium map in html format
+
+Map1.convertToAdjacencyList()
